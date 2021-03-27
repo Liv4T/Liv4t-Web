f925c9c697d624f47bc6e7d3aad39e67c73c460e
Gabriel Di Tizio <gaboditizio@gmail.com>
2021-03-27 18:13:17 -0500

    Cambios

--- public/91.js
+++ public/91.js
@@ -1,13 +1,13 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([[91],{
 
-/***/ "./resources/js/components/menuPadres.vue":
-/*!************************************************!*\
-  !*** ./resources/js/components/menuPadres.vue ***!
-  \************************************************/
+/***/ "./resources/js/components/parents.vue":
+/*!*********************************************!*\
+  !*** ./resources/js/components/parents.vue ***!
+  \*********************************************/
 /*! exports provided: default */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed (from ./node_modules/vue-loader/lib/index.js):\nError: ENOENT: no such file or directory, open '/var/www/html/Liv4t-Web/resources/js/components/menuPadres.vue'");
+throw new Error("Module build failed (from ./node_modules/vue-loader/lib/index.js):\nError: ENOENT: no such file or directory, open '/var/www/html/Liv4t-Web/resources/js/components/parents.vue'");
 
 /***/ })
 
--- public/94.js
+++ public/94.js
@@ -1,13 +1,13 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([[94],{
 
-/***/ "./resources/js/components/Comunicates.vue":
-/*!*************************************************!*\
-  !*** ./resources/js/components/Comunicates.vue ***!
-  \*************************************************/
+/***/ "./resources/js/components/reportVisits.vue":
+/*!**************************************************!*\
+  !*** ./resources/js/components/reportVisits.vue ***!
+  \**************************************************/
 /*! exports provided: default */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed (from ./node_modules/vue-loader/lib/index.js):\nError: ENOENT: no such file or directory, open '/var/www/html/Liv4t-Web/resources/js/components/Comunicates.vue'");
+throw new Error("Module build failed (from ./node_modules/vue-loader/lib/index.js):\nError: ENOENT: no such file or directory, open '/var/www/html/Liv4t-Web/resources/js/components/reportVisits.vue'");
 
 /***/ })
 
